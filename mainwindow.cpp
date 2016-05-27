eccd0a5f53a75dc58d9099b165808be576049264
gaunah <christoph@zelinski.de>
2016-05-27 16:20:58 +0200

    ui, handle ok button, getWeight()

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
+void MainWindow::startNewGame(){
+    qDebug() << "not implemented!";
+}
+
 void MainWindow::updateOKButtonState()
 {
-    ui->pushButton_ok->setDisabled(ui->lineEdit_guess->text().isEmpty());
+    ui->pushButton_ok->setDisabled(ui->lineEdit_guess->text().trimmed().isEmpty());
+    ui->lineEdit_guess->setStyleSheet(""); //set default StyleSheet
+}
+
+void MainWindow::handleOKButton()
+{
+    int weight = getWeight(ui->lineEdit_guess->text().trimmed());
+    if (weight != -1) {
+
+    } else {
+        //set lineEdit background lightred
+        ui->lineEdit_guess->setStyleSheet("background-color: #F5A9A9");
+    }
 }
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -11,6 +29,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton_ok, SIGNAL(clicked(bool)), this, SLOT(handleOKButton()));
+    connect(ui->actionQuit, SIGNAL(triggered(bool)), qApp, SLOT(quit()));
+    connect(ui->actionNew_game, SIGNAL(triggered(bool)), this, SLOT(startNewGame()));
+
     updateOKButtonState();
 }
 
@@ -20,24 +42,29 @@ MainWindow::~MainWindow()
 }
 
 /*!
- * \brief MainWindow::getGuess
- * \return
- */
-double MainWindow::getGuess() const
-{
-
-}
-
-/*!
- * \brief MainWindow::setImgName
- * \param name
+ * \brief MainWindow::setImgName sets the image lable
+ * \param name of the image
  */
 void MainWindow::setImgName(const QString &name)
 {
-
+    ui->label_imgName->setText(name.trimmed());
 }
 
-double MainWindow::getWeight(const QString &input)
+int MainWindow::getWeight(const QString &input) const
 {
+    QRegularExpression regex("\\A([1-9][0-9]{0,2})[ ]?(mg|g|kg)\\z");
+    regex.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
+    QRegularExpressionMatch match = regex.match(input);
 
+    if (match.hasMatch()) {
+        double weight = match.captured(1).toDouble();
+        const QString unit = match.captured(2).toLower();
+        if (unit == "g") {
+            weight *= 1000;
+        } else if (unit == "kg") {
+            weight *= 1000000;
+        }
+        return weight;
+    }
+    return -1; //no valid match
 }
--- mainwindow.h
+++ mainwindow.h
@@ -12,18 +12,21 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public slots:
+    void startNewGame();
+
+private slots:
     void updateOKButtonState();
+    void handleOKButton();
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-    double getGuess() const;
     void setImgName(const QString &name);
 
 private:
     Ui::MainWindow *ui;
-    double getWeight(const QString &input);
+    int getWeight(const QString &input) const;
 };
 
 #endif // MAINWINDOW_H
